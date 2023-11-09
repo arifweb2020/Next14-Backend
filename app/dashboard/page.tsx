f79c03b9fd53bcea1a0077c8598c66b1c6f5caf1
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-09 17:54:47 +0530

    cards design

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
-        <div className={styles.card}>
+        <div className={styles.cards}>
           <Card/>
           <Card/>
           <Card/>
--- app/ui/dashboard/card/card.tsx
+++ app/ui/dashboard/card/card.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import styles from './card.module.css'
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
 
 const Card = () => {
   return (
-    <div>card</div>
-  )
-}
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total users</span>
+        <span className={styles.number}>10,3445</span>
+        <span className={styles.detail}>
+          <span className={styles.negative}>
+            90%
+          </span>{" "}
+          less than previous week
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
