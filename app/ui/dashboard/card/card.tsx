8399c4f51d3afc4dca7b8d2721d1ac6ac6b5e8b7
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-09 17:46:46 +0530

    dashboard layout done

---
+++ app/ui/dashboard/card/card.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './card.module.css'
+
+const Card = () => {
+  return (
+    <div>card</div>
+  )
+}
+
+export default Card
---
+++ app/ui/dashboard/chart/chart.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './chart.module.css'
+
+const Chart = () => {
+  return (
+    <div>chart</div>
+  )
+}
+
+export default Chart
