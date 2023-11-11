4d4541e3bc3713232d3bbc937d9979ff52741242
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-11 12:46:34 +0530

    pagination done

--- app/ui/dashboard/pagination/pagination.tsx
+++ app/ui/dashboard/pagination/pagination.tsx
@@ -11,7 +11,7 @@ const Pagination:React.FC<CountProps> = ({ count }) => {
   const { replace } = useRouter();
   const pathname = usePathname();
 
-  const page : any = searchParams.get("page") || 1;
+  const page : any | string = searchParams.get("page") || 1;
 
   const params = new URLSearchParams(searchParams);
   const ITEM_PER_PAGE = 2;
@@ -19,26 +19,32 @@ const Pagination:React.FC<CountProps> = ({ count }) => {
   const hasPrev = ITEM_PER_PAGE * (parseInt(page) - 1) > 0;
   const hasNext = ITEM_PER_PAGE * (parseInt(page) - 1) + ITEM_PER_PAGE < count;
 
-//   const handleChangePage = (type) => {
-//     type === "prev"
-//       ? params.set("page", parseInt(page) - 1)
-//       : params.set("page", parseInt(page) + 1);
-//     replace(`${pathname}?${params}`);
-//   };
+  // const handleChangePage = (type:any) => {
+  //   type === "prev"
+  //     ? params.set("page", parseInt(page) - 1)
+  //     : params.set("page", parseInt(page) + 1);
+  //   replace(`${pathname}?${params}`);
+  // };
+
+  const handleChangePage = (type: any) => {
+    const newPage = type === "prev" ? parseInt(page) - 1 : parseInt(page) + 1;
+    params.set("page", newPage.toString()); // Convert the number to a string
+    replace(`${pathname}?${params}`);
+  };
 
   return (
     <div className={styles.container}>
       <button
         className={styles.button}
         disabled={!hasPrev}
-       // onClick={() => handleChangePage("prev")}
+        onClick={() => handleChangePage("prev")}
       >
         Previous
       </button>
       <button
         className={styles.button}
         disabled={!hasNext}
-       // onClick={() => handleChangePage("next")}
+        onClick={() => handleChangePage("next")}
       >
         Next
       </button>
