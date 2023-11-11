61c0628e5fc7d2f5b3cfa768e2fee16258f61b95
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-11 12:49:24 +0530

    single user data  fetch

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,11 +1,11 @@
 // import { updateUser } from "@/app/lib/actions";
-// import { fetchUser } from "@/app/lib/data";
+ import { fetchUser } from "@/app/lib/data";
 import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
 import Image from "next/image";
 
-const SingleUserPage = async () => {
-  //   const { id } = params;
-  //   const user = await fetchUser(id);
+const SingleUserPage = async ({params}:any) => {
+    const { id } = params;
+    const user :any= await fetchUser(id);
 
   return (
     <div className={styles.container}>
@@ -18,7 +18,7 @@ const SingleUserPage = async () => {
       </div>
       <div className={styles.formContainer}>
         {/* <form action={updateUser} className={styles.form}> */}
-        {/* <form className={styles.form}>
+        <form className={styles.form}>
           <input type="hidden" name="id" value={user.id} />
           <label>Username</label>
           <input type="text" name="username" placeholder={user.username} />
@@ -29,7 +29,7 @@ const SingleUserPage = async () => {
           <label>Phone</label>
           <input type="text" name="phone" placeholder={user.phone} />
           <label>Address</label>
-          <textarea type="text" name="address" placeholder={user.address} />
+          <textarea  name="address" placeholder={user.address} />
           <label>Is Admin?</label>
           <select name="isAdmin" id="isAdmin">
             <option value="true" selected={user.isAdmin}>
@@ -49,39 +49,8 @@ const SingleUserPage = async () => {
             </option>
           </select>
           <button>Update</button>
-        </form> */}
-        <form className={styles.form}>
-          <input type="hidden" name="id" value="arif" />
-          <label>Username</label>
-          <input type="text" name="username" placeholder="Arif" />
-          <label>Email</label>
-          <input type="email" name="email" placeholder="arif@gmail.com" />
-          <label>Password</label>
-          <input type="password" name="password" />
-          <label>Phone</label>
-          <input type="text" name="phone" placeholder="98772e2332" />
-          <label>Address</label>
-          <textarea  name="address" placeholder="sdfds sdggs dsg" />
-          <label>Is Admin?</label>
-          <select name="isAdmin" id="isAdmin">
-            <option value="true" >
-              Yes
-            </option>
-            <option value="false">
-              No
-            </option>
-          </select>
-          <label>Is Active?</label>
-          <select name="isActive" id="isActive">
-            <option value="true" >
-              Yes
-            </option>
-            <option value="false">
-              No
-            </option>
-          </select>
-          <button>Update</button>
         </form>
+       
       </div>
     </div>
   );
